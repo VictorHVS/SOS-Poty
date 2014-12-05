0290f1350a8a8dae1ff8ffb6cab6333c6a893a02
Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>
2014-12-05 09:58:02 -0300

    Importado Cena, Animacao do Loaldo e mecanica implementada

---
+++ Assets/Scripts/BarcoMovimento.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarcoMovimento : MonoBehaviour {
+	
+	public float velocity;
+
+	private Vector3 move = Vector3.zero;
+
+	private bool isMovingLeft;
+	private bool isMovingRight;
+	
+	void Start () {
+		isMovingLeft = false;
+		isMovingRight = false;
+
+		move = transform.localPosition;
+	}
+
+	void Update () {
+
+		print (move.x);
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			isMovingLeft = true;
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			isMovingRight = true;
+		}
+
+		if (isMovingRight && transform.localPosition.x < 1 && !isMovingLeft) {
+			move.x += velocity;
+		}
+		if (isMovingLeft && transform.localPosition.x > -1 && !isMovingRight) {
+			move.x -= velocity;
+		}
+
+		if (move.x > 0.9f || move.x < -0.9f) {
+			isMovingRight = false;
+			isMovingLeft = false;
+		}
+
+		if(move.x > 0f && move.x < 0.01f || move.x < 0f && move.x > -0.01f) {
+			isMovingRight = false;
+			isMovingLeft = false;
+		}
+
+		transform.localPosition = move;
+	}
+}
---
+++ Assets/Scripts/SplineTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineTest : MonoBehaviour {
+
+	public Transform[] path;
+	public float speed = 0.5f;
+	
+	float t = 0;
+	private SmoothQuaternion sr;
+	Quaternion q;
+	
+	void Start() {
+		sr = transform.rotation;
+		sr.Duration = 0.5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Spline.MoveOnPath(path, transform.position, ref t, ref q, speed, 50, EasingType.Sine, true, true);
+		sr.Value = q;
+		transform.rotation = sr;
+	}
+}
